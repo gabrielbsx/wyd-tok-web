a196ca7e5a11b7fd03c4352012471ce94835a947
gabrielbsx <gabrielturing@gmail.com>
2021-01-01 17:51:51 -0300

    Ajuste na view para o join

--- app/Views/dashboard/pages/donation.php
+++ app/Views/dashboard/pages/donation.php
@@ -13,32 +13,37 @@
         <div class="text-white grid grid-cols-4 bg-gray-900 py-4 mb-3 px-3 rounded-b-lg -my-2">
             <?php if ($donate_paginate) : ?>
                 <?php foreach ($donate_paginate as $key => $value) : ?>
+                    <?php if ($value['createdAt'] == null): ?>
+                        <div class="text-center col-span-4">
+                            Não há faturas  
+                        </div>
+                    <?php $auth = true; continue; endif; ?>
                     <div class="col-span-4 grid grid-cols-8 my-1 px-1 py-4 px-6 bg-gray-800 border-b-4 border-gray-900 text-gray-300 rounded-lg bg-black">
                         <div class="col-span-4 px-1 py-2">
                             <span class="text-left">
                                 <span class="text-left">
-                                    R$ <?= $value['picpay_value'] ?? $value['mercadopago_value'] ?>,00
+                                    R$ <?= $value['value'] ?>
                                 </span>
                             </span>
                         </div>
                         <div class="col-span-2 px-1 py-2">
                             <span class="text-left">
-                                <?= ($value['picpay_createdAt'] ?? $value['picpay_updatedAt']) ?? ($value['mercadopago_createdAt'] ?? $value['mercadopago_updatedAt']) ?>
+                                <?= $value['createdAt'] ?? $value['updatedAt'] ?>
                             </span>
                         </div>
                         <div class="col-span-2 py-2 text-right">
-                            <?php if (!($value['picpay_status'] || $value['mercadopago_status'])) : ?>
-                                <a target="_blank" href="<?= $value['picpay_paymentUrl'] ?? $value['mercadopago_paymentUrl'] ?>" class="col-span-1 px-12 mr-2 py-2 rounded-lg border-b-4 bg-green-700 hover:bg-green-600 hover:border-green-800 border-green-900">
+                            <?php if (!($value['status'])) : ?>
+                                <a target="_blank" href="<?= $value['paymentUrl'] ?>" class="col-span-1 px-12 mr-2 py-2 rounded-lg border-b-4 bg-green-700 hover:bg-green-600 hover:border-green-800 border-green-900">
                                     Pagar
                                 </a>
                             <?php endif; ?>
-                            <span class="col-span-1 px-12 py-2 rounded-lg border-b-4 <?= $value['picpay_status'] || $value['mercadopago_status'] ? 'bg-green-700 hover:bg-green-600 hover:border-green-800 border-green-900' : 'bg-yellow-700 hover:bg-yellow-600 hover:border-yellow-800 border-yellow-900' ?>">
-                                <?= $value['picpay_status'] || $value['mercadopago_status'] ? 'Pago' : 'Pendente' ?>
+                            <span class="col-span-1 px-12 py-2 rounded-lg border-b-4 <?= $value['status'] ? 'bg-green-700 hover:bg-green-600 hover:border-green-800 border-green-900' : 'bg-yellow-700 hover:bg-yellow-600 hover:border-yellow-800 border-yellow-900' ?>">
+                                <?= $value['status'] ? 'Pago' : 'Pendente' ?>
                             </span>
                         </div>
                     </div>
                 <?php endforeach; ?>
-                <?php if ($donate_pager) : ?>
+                <?php if ($donate_pager && !isset($auth)) : ?>
                     <?= $donate_pager->links('donate', 'pagination') ?>
                 <?php endif; ?>
             <?php else : ?>
@@ -63,7 +68,7 @@
                                         <img src="https://s3.amazonaws.com/preview.thegamecrafter.com/7C2E1DC8-245A-11EA-AA99-974C4E5FF538.png" class="w-3/5" />
                                     </div>
                                     <p class="text-center bg-black py-2 font-bold text-1xl rounded-lg">
-                                        <?= $value['value'] ?> + <?= $value['bonus'] ?>% = <?= $value['value'] + ($value['value'] * ($value['bonus'] / 100)) ?> donate
+                                        <?= $value['value'] ?> + <?= $value['bonus'] ?>% = <?= ceil($value['value'] + ($value['value'] * ($value['bonus'] / 100))) ?> donate
                                     </p>
                                     <?php if ($value['donate_bonus']) : ?>
                                         <div class="text-center mt-2 bg-black font-bold text-1xl rounded-lg">
